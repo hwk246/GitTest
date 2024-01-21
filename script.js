2e2c04e4231d37ff56752037a2ae888e68c73682
Ron Klinker <r.klinker@gmail.com>
2024-01-21 15:55:01 +0100

    added a button for alert

--- script.js
+++ script.js
@@ -2,3 +2,8 @@ const clickHeader = document.querySelector('#index-header')
 clickHeader.addEventListener('click',()=>{
     clickHeader.style.color = 'white'
 })
+
+const clickButton = document.querySelector('button')
+clickButton.addEventListener('click', () => {
+    alert('hallo')
+})
